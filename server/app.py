11b5a9290a0d80741f87297c17d39ffa9e1b3c60
Olivia Nordquist <nolivia@umich.edu>
2015-03-29 13:28:37 -0400

    saving uploads incorrectly

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+import os
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/start_job', methods=['POST'])
 def start_job():
     f = request.files['script_file']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     print f
     return 'Hello World!'
 

6787bbde37b836ee43add427810d41efe0a2fbdb
Olivia Nordquist <nolivia@umich.edu>
2015-04-01 21:32:27 -0400

    added a read

---
+++ README.txt
@@ -0,0 +1,20 @@
+Installing dependencies:
+
+pip install -r requirements.txt
+
+Start the server by running
+
+python server/app.py
+
+In a separate terminal, to add a job run
+
+python client/mr_manager.py add path/to/my/job
+
+This will return the job ID immediately.
+
+To get a status of a job, run
+
+python client/mr_manager status {job ID}
+
+sample_job is provided as an example for running bash scripts but the shebang
+can be changed to run different programs such as a python script.
--- client/mr_manager.py
+++ client/mr_manager.py
@@ -11,7 +11,11 @@ if argv[1] == 'add':
     file = open(argv[2], 'rb')
     files = {'script_file': file}
 
-    r = requests.post(url, files=files)
+    try:
+        r = requests.post(url, files=files)
+    except requests.exceptions.ConnectionError:
+        print 'Server Not Running'
+        exit(-1)
 
     print r.text
 
@@ -21,9 +25,15 @@ elif argv[1] == 'status':
     url = 'http://127.0.0.1:5000/status'
     data = {'req_pid': pid}
 
-    r = requests.get(url, params=data)
+    try:
+        r = requests.get(url, params=data)
+    except requests.exceptions.ConnectionError:
+        print 'Server Not Running'
+        exit(-1)
 
     print r.text
 
 else:
-    pass
+
+    print 'Bad Command!'
+    exit(-1)
